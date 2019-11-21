b4316f2fac71c7bd04ec58e2cdd92fa0d6c56d4e
Raisa Bustos <raisa.bustosv@gmail.com>
2019-11-20 23:26:22 -0500

    cleaning code adding  macro to index.html

--- application.py
+++ application.py
@@ -11,6 +11,7 @@ def registration():
     user_form = userForm()
     if user_form.validate_on_submit():
         return "Great success!"
+        
     return render_template("index.html", form=user_form)
 
 if __name__ == "__main__":
